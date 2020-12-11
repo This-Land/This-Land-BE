0c9380bffc60271d6050437e8fba9682b0842cea
trident6 <philip.johnson.it@gmail.com>
2020-12-11 17:54:52 -0500

    env key

--- landbe/landapp/settings.py
+++ landbe/landapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j&$^!%4l=s52vmgu&s(id&acxznm+s6#6j2=))$t1m0bnv7x!b'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
