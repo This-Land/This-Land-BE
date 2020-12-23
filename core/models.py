e376c8847253087b2bde056866e863106f664acd
babacardiouf544 <babacardi@gmail.com>
2020-12-23 15:12:07 -0500

    poi FK

--- core/models.py
+++ core/models.py
@@ -22,10 +22,11 @@ class PointOfInterest(models.Model):
 
 
 class TellYourStory(models.Model):
-    #user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='TellYourStories')    
+    poi = models.ForeignKey(to=PointOfInterest, on_delete=models.CASCADE, related_name='TellYourStories')    
     text = models.TextField(max_length=255, null=True, blank=True)
     images = models.ImageField(upload_to='media/images/', null=True)
     date_created = models.DateField(auto_now=True)
+
 
     # def __str__(self):
     #     return f"{self.text} {self.images} {self.date_created}"
