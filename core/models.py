a0cfd64c2e6c84b85471b307d30910790e35c147
trident6 <philip.johnson.it@gmail.com>
2020-12-28 10:52:46 -0500

    tys poi fk added

--- core/models.py
+++ core/models.py
@@ -23,6 +23,7 @@ class PointOfInterest(models.Model):
 
 class TellYourStory(models.Model):
     #user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='TellYourStories')    
+    poi = models.ForeignKey(to=PointOfInterest, on_delete=models.CASCADE, related_name='TellYourStories', null=True)  
     text = models.TextField(max_length=255, null=True, blank=True)
     images = models.ImageField(upload_to='media/images/', null=True)
     date_created = models.DateField(auto_now=True)
