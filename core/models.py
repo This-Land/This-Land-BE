2a617d95f6a0eb6dba2e1e23b585b98b8fb53116
trident6 <philip.johnson.it@gmail.com>
2021-01-04 22:58:59 -0500

    redirect to admin

--- core/models.py
+++ core/models.py
@@ -18,6 +18,9 @@ class PointOfInterest(models.Model):
     category = models.CharField(max_length=35)
     date_created = models.DateField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.location_name}" 
+
 
 class TellYourStory(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='TellYourStories', null=True)
