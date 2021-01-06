aa3cd9b123d1d6a99f94b9fab8dc67cabb5b37ec
babacardiouf544 <babacardi@gmail.com>
2021-01-06 14:22:12 -0500

    edited settings.py

--- landapp/settings.py
+++ landapp/settings.py
@@ -162,10 +162,10 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #    'rest_framework.authentication.TokenAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
          
-    # ),
+    ),
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
         
