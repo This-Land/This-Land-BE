5873f8a926fd9278fa972ab6a512a0b87f4a1fc5
trident6 <philip.johnson.it@gmail.com>
2021-01-07 10:38:47 -0500

    changed DIRS in settings

--- landapp/settings.py
+++ landapp/settings.py
@@ -87,7 +87,7 @@ ROOT_URLCONF = 'landapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
